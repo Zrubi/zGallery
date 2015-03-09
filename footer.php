55f5269cf7a5fe525a90d0c205d9c967f6895e5d
Zrubi <mail@zrubi.hu>
2015-03-09 21:56:16 +0100

    basic template with a sticky header & footer

--- footer.php
+++ footer.php
@@ -1,9 +1,15 @@
 <div class="clear"></div>
 </div>
 <footer id="footer" role="contentinfo">
+
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'zgallery' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'zgallery' ), '<a href="http://zrubi.hu/">Zrubi</a>' ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s - All Rights Reserved', 'zgallery' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 </div>
+
+<div id="credits">
+<?php echo sprintf( __( ' Theme Designed by: %1$s', 'zgallery' ), '<a href="http://zrubi.hu/">Zrubi</a>' ); ?>
+</div>
+
 </footer>
 </div>
 <?php wp_footer(); ?>
